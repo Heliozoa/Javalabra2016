b55a43fb778c47c6ca88ef99e1f1165c3a4b7aa3
Heliozoa <dm89132@gmail.com>
2016-01-02 22:42:45 +0200

    korjaus

---
+++ Lukulista/src/main/java/oma/lukulista/kayttoliittyma/Tekstikayttoliittyma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oma.lukulista.kayttoliittyma;
+
+/**
+ *
+ * @author authority
+ */
+public class Tekstikayttoliittyma implements Kayttoliittyma{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
